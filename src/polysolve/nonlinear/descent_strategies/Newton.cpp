974aa202b2d1eb0034c5e3df749656120a414200
maxpaik16 <maxpaik@gmail.com>
2024-12-18 17:15:24 -0500

    clean up code and add timing

--- src/polysolve/nonlinear/descent_strategies/Newton.cpp
+++ src/polysolve/nonlinear/descent_strategies/Newton.cpp
@@ -200,7 +200,7 @@ namespace polysolve::nonlinear
                 // Eigen::saveMarket(hessian, "problematic_hessian.mtx");
                 return std::nan("");
             }
-            linear_solver->set_nullspace(nullspace);
+            linear_solver->set_positions(positions);
             linear_solver->solve(-grad, direction); // H Δx = -g
         }
 
@@ -241,7 +241,7 @@ namespace polysolve::nonlinear
             {
                 linear_solver->analyze_pattern_dense(hessian, hessian.rows());
                 linear_solver->factorize_dense(hessian);   
-                linear_solver->set_nullspace(nullspace);
+                linear_solver->set_positions(positions);
                 linear_solver->solve(-grad, direction);
             }
             catch (const std::runtime_error &err)
